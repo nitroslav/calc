4005130a04339c09e226cef7186b34deb4a4442e
tlkmy <tlkmy@NITRODESKTOP>
2023-04-13 22:59:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в КАЛЬКУЛЯТОР");
+            Iaddition addition = new addition();
+            addition.add();
+        }
+        public interface Iaddition
+        {
+            
+            void add()
+            {
+                
+            }
+        }
+        public class addition : Iaddition
+        {
+            public void add()
+            {
+                int number1;
+                int number2;
+                Console.Write("Введите число 1: ");
+                number1 = int.Parse(Console.ReadLine());
+                Console.Write("Введите число 2: ");
+                number2 = int.Parse(Console.ReadLine());
+                Console.Write("Результат: ");
+                Console.Write(number1 + number2);
+            }
+        }
+    }
+}
